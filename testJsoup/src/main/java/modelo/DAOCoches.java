4f008d171c5a4fabbf99da3ed5e4fcf01deba224
06268818 <06268818@DESKTOP-U8E752U>
2019-11-29 12:08:41 +0100

    Añadido parte visual y spring boot

--- testJsoup/src/main/java/modelo/DAOCoches.java
+++ testJsoup/src/main/java/modelo/DAOCoches.java
@@ -9,12 +9,13 @@ import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.or;
 import static com.mongodb.client.model.Filters.lt;
 import static com.mongodb.client.model.Filters.gt;
-
+import static com.mongodb.client.model.Filters.gte;
 import static com.mongodb.client.model.Updates.set;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import modelo.MongoBroker;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 
@@ -22,21 +23,57 @@ public class DAOCoches {
 
 	private static MongoCollection<Document> dbCoches=MongoBroker.get().getCollection("Coches");
 	private static String coleccionCoches="Coches";
-
+	public static List<Document> listaCoches;	
+	
 	public static void insert(Document coche) {
-		dbCoches=MongoBroker.getCollection(coleccionCoches);
+		//dbCoches=MongoBroker.getCollection(coleccionCoches);
 		dbCoches.insertOne(coche);
 	}
-	public static void insertList(List<Document>coches) {
-		dbCoches=MongoBroker.getCollection(coleccionCoches);
-		dbCoches.insertMany(coches);
+	
+	public static void deleteAll() {
+		dbCoches.drop();
 	}
 	
-	public static List<Document> selectRange(float minPrecio,float maxPrecio) {
-		//genera un filtro para la busqueda por precio
-		Bson filtroRango=and(gt("precio",minPrecio),lt("precio",maxPrecio));
+	public static List<Document> getAllCars() {		
+		FindIterable<Document> resultados= dbCoches.find();
+		listaCoches = new ArrayList<Document>();
+		for(Document doc: resultados) {
+			listaCoches.add(doc);
+		}
+		return listaCoches;
+	}	
+	
+	public static List<Document> advance(float minPrecio,float maxPrecio,String marca, String modelo){
+		//b�squeda base (todos los coches con un precio mayor a cero u otro valor)
+		Bson filtro= and(gt("precio",minPrecio));
 		
-		FindIterable<Document> resultados= dbCoches.find(filtroRango);
+		if(maxPrecio > 0) {
+			//busqueda en un rango de precios
+			filtro= and(gt("precio",minPrecio),lt("precio",maxPrecio));
+			if(marca != "" ) {
+				//busqueda en un rango de precio y marca definida
+				filtro= and(gt("precio",minPrecio),lt("precio",maxPrecio),
+						  and(eq("marca",marca)));
+				if(modelo!= "") {
+					//busqueda en un rango de precio, con marca y modelo definidos
+					filtro= and(gt("precio",minPrecio),lt("precio",maxPrecio),
+							  and(eq("marca",marca), and(eq("modelo",modelo))));
+				}
+			}
+		}else {
+			if(marca != "" ) {
+				filtro= and(gte("precio",minPrecio),and(eq("marca",marca)));
+				if(modelo!= "") {
+					filtro= and(gte("precio",minPrecio),and(eq("marca",marca), and(eq("modelo",modelo))));
+				}
+			}else {
+				if(modelo!= "") {
+					filtro= and(gte("precio",minPrecio), and(eq("modelo",modelo)));
+				}
+			}
+		}
+		
+		FindIterable<Document> resultados=dbCoches.find(filtro);
 		List<Document> coches = new ArrayList<Document>();
 		for(Document doc: resultados) {
 			coches.add(doc);
@@ -44,14 +81,27 @@ public class DAOCoches {
 		return coches;
 	}
 	
-	//metodo para buscar los coches por su marca
-	public static List<Document> selectFirm(String name){
-		Bson filtroMarca=eq("marca",name);
-		FindIterable<Document> resultados=dbCoches.find(filtroMarca);
-		List<Document> coches = new ArrayList<Document>();
-		for(Document doc: resultados) {
-			coches.add(doc);
+	public static ArrayList<String> obtenerMarcasCoches() {
+		ArrayList<String> marcas = new ArrayList<String>();
+		for(Document doc: listaCoches) {
+			marcas.add(doc.getString("marca"));
 		}
-		return coches;
+		return marcas;
+	}
+	
+	public static ArrayList<String> obtenerModelosCoches() {
+		ArrayList<String> modelos = new ArrayList<String>();
+		for(Document doc: listaCoches) {
+			modelos.add(doc.getString("modelo"));
+		}	    
+	    return modelos;
+	}
+	
+	public static ArrayList<Integer> obtenerKilometrajes() {
+		ArrayList<Integer> kms = new ArrayList<Integer>();
+		for(Document doc: listaCoches) {
+			kms.add(Integer.parseInt(doc.getString("kilometraje")));
+		}	    
+	    return kms;
 	}
 }
